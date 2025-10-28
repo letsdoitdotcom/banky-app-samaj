12b813f28ff53acde79d183d99e76586626ab748
PaulKelvin <Ibukunadesanya0@gmail.com>
2025-10-28 19:14:18 +0100

    Fix homepage redirect from /admin/dashboard to /admin

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   useEffect(() => {
     if (isAuthenticated) {
       if (admin) {
-        router.push('/admin/dashboard');
+        router.push('/admin');
       } else if (user) {
         router.push('/dashboard');
       }
